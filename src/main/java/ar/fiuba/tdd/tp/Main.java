06216432cbb63f9267bd099e367051bf3c1bb405
Bruno Liberini <bliberini@gmail.com>
2016-09-18 17:37:02 -0300

    Primer intento configuración

---
+++ src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -0,0 +1,10 @@
+package ar.fiuba.tdd.tp;
+
+/**
+ * Created by bliberini on 18/09/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is just a template project");
+    }
+}
---
+++ src/test/java/ar/fiuba/tdd/tp/MainTests.java
@@ -0,0 +1,16 @@
+package ar.fiuba.tdd.tp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bliberini on 18/09/2016.
+ */
+public class MainTests {
+
+    @Test
+    public void test() {
+        assertEquals(0,0);
+    }
+}
