32042b45ead55923b3b0638d021c733c08b755bc
marcosRiarte <mriarte@fi.uba.ar>
2016-10-07 16:47:06 -0300

    Implements ValidateTest for RuleNoRepeteadValuesTest

--- src/test/java/ar/fiuba/tdd/tp1/model/RuleNoRepeatedValuesTest.java
+++ src/test/java/ar/fiuba/tdd/tp1/model/RuleNoRepeatedValuesTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Map;
 import java.util.Objects;
 
 import static org.junit.Assert.*;
@@ -16,7 +17,10 @@ import static org.junit.Assert.*;
 public class RuleNoRepeatedValuesTest {
     private BoardRectangularWithRegions oneBoard;
     private RuleNoRepeatedValues oneRule;
-    private List<Object> listOfConflictingCellIds;
+    private RuleNoRepeatedValues otherRule;
+    private RuleNoRepeatedValues oneMoreRule;
+
+    private List<Integer> listOfConflictingCellIds;
     private List<Object> listParams;
     private Move moving;
     private CellNumerical numericCell;
@@ -24,19 +28,66 @@ public class RuleNoRepeatedValuesTest {
     private Object visitingCellValue = null;
     private Integer integer1;
     private ArrayList<ViolationOfRule> violatedRules;
+    private CellNumerical numericCell1;
+    private CellNumerical numericCell2;
+    private CellNumerical numericCell3;
+    private CellNumerical numericCell4;
+    private Region validRegion;
+    private Region invalidRegion;
+    private List<String> listCelds;
+    private List<Integer> listIdCelds;
+    private Map<String, Region> regionsMap;
 
     @Before
     public void setUp() throws Exception {
         this.integer1 = new Integer(4);
         this.oneBoard = new BoardRectangularWithRegions(9, 9);
-        this.oneRule = new RuleNoRepeatedValues(oneBoard, "6");
+        this.oneRule = new RuleNoRepeatedValues(oneBoard, "1");
         this.numericCell = new CellNumerical(integer1, "4");
+
         this.alfabeticCell = new CellAlphabetical("a", "3");
+
         this.moving = new Move(new Integer(5), numericCell);
         this.listParams = new ArrayList<Object>();
         this.listParams.add("18");
-       // this.listOfConflictingCellIds.add("18");
-        //this.violatedRules.add(new ViolationOfRule("hay valores repetidos",this.listOfConflictingCellIds));
+        this.listOfConflictingCellIds = new ArrayList<Integer>();
+        this.listOfConflictingCellIds.add(new Integer(11));
+        this.listOfConflictingCellIds.add(new Integer(18));
+
+        this.numericCell1 = new CellNumerical(new Integer(2),"1");
+        this.numericCell2 = new CellNumerical(new Integer(2),"2");
+        this.numericCell3 = new CellNumerical(new Integer(1),"3");
+        this.numericCell4 = new CellNumerical(new Integer(4),"4");
+
+        this.listCelds = new ArrayList<String>();
+        this.listCelds.add("1");
+        this.listCelds.add("2");
+        this.listCelds.add("3");
+        this.listCelds.add("4");
+
+        this.listIdCelds = new ArrayList<Integer>();
+        this.listIdCelds.add(new Integer(1));
+        this.listIdCelds.add(new Integer(2));
+        this.listIdCelds.add(new Integer(3));
+        this.listIdCelds.add(new Integer(4));
+
+
+        this.validRegion = new Region(listCelds,"9");
+        this.invalidRegion = new Region(listCelds,"8");
+
+        this.oneBoard.setCellById(new Integer(1),numericCell1);
+        this.oneBoard.setCellById(new Integer(2),numericCell2);
+        this.oneBoard.setCellById(new Integer(3),numericCell3);
+        this.oneBoard.setCellById(new Integer(4),numericCell4);
+
+        this.oneBoard.setRegion("1",this.validRegion);
+        this.oneBoard.setRegion("2",this.invalidRegion);
+
+        this.otherRule = new RuleNoRepeatedValues(this.oneBoard,"1");
+        this.oneMoreRule = new RuleNoRepeatedValues(this.oneBoard,"2");
+
+
+        // this.violatedRules = new ArrayList<ViolationOfRule>("suma incorrecta",this.listOfConflictingCellIds);
     }
 
     @Test
@@ -46,9 +97,9 @@ public class RuleNoRepeatedValuesTest {
 
     @Test
     public void validateTest() throws Exception {
-        //this.numericCell = new CellNumerical(new Integer(0),"3");
-        //assertEquals(3,this.listOfConflictingCellIds.size());
-        //assertEquals(false,oneRule.validate(moving));
+        assertTrue(this.moving.getListOfViolationsOfRules().isEmpty());
+        this.oneRule.validate(this.moving);
+        assertEquals(0,moving.getListOfViolationsOfRules().size());
     }
 
     @Test
@@ -74,7 +125,7 @@ public class RuleNoRepeatedValuesTest {
     @Test
     public void visitTest() throws Exception {
         this.oneRule.visit(this.alfabeticCell);
-        assertEquals("a", ((String) oneRule.getVisitingCellValue()));
+        assertEquals("a",((String)oneRule.getVisitingCellValue()));
     }
 
     @Test
@@ -92,7 +143,7 @@ public class RuleNoRepeatedValuesTest {
 
     @Test
     public void getRegionIdTest() throws Exception {
-        assertEquals("6", this.oneRule.getRegionId());
+        assertEquals("1", this.oneRule.getRegionId());
     }
 
 }
