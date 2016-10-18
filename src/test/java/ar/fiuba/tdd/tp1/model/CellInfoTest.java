b305778ac34952519b46e47dee15608a8a1bdd15
marcosRiarte <mriarte@fi.uba.ar>
2016-10-18 18:03:53 -0300

    CellInfoTest

---
+++ src/test/java/ar/fiuba/tdd/tp1/model/CellInfoTest.java
@@ -0,0 +1,45 @@
+package ar.fiuba.tdd.tp1.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *This class is for the unit test of class InfoCell.
+ The getId method, you should return the cell ID.
+ The getValue method should return the value of the cell.
+ The isEditable method should return a Boolean condition that
+ does not say whether the cell is editable or not.
+ */
+public class CellInfoTest {
+    private CellInfo cellInfo;
+    int cellId;
+    String cellValue;
+    Boolean isEditableCell;
+
+    @Before
+    public void setUp() throws Exception {
+        this.cellId = 1;
+        this.cellValue = "2";
+        this.isEditableCell = false;
+        this.cellInfo = new CellInfo(cellId,this.cellValue,this.isEditableCell);
+
+    }
+
+    @Test
+    public void getId() throws Exception {
+        assertEquals(this.cellId,this.cellInfo.getId());
+    }
+
+    @Test
+    public void getValue() throws Exception {
+        assertEquals(this.cellValue,this.cellValue);
+    }
+
+    @Test
+    public void isEditable() throws Exception {
+        assertEquals(this.isEditableCell,this.cellInfo.isEditable());
+    }
+
+}
