35849cb490980bbc600f15ec6aee7ded50e856b1
nico <n.rogica@gmail.com>
2016-10-20 13:34:51 -0300

    fix end game cycle verification

--- src/main/java/ar/fiuba/tdd/tp1/model/EndGameNoCycles.java
+++ src/main/java/ar/fiuba/tdd/tp1/model/EndGameNoCycles.java
@@ -8,12 +8,12 @@ import ar.fiuba.tdd.tp1.model.interfaces.EndGameCondition;
 public class EndGameNoCycles implements EndGameCondition {
 
     public Boolean validate(BoardRectangularWithRegions board) {
-        for (int i = 1;i == board.getRowQuantity() - 1;i ++ ) {
-            for (int j = 1;j == board.getColumnQuantity() - 1;j ++ ) {
-                Cell firstCell = board.getCellByCoordinates(i,j);
-                Cell secondCell = board.getCellByCoordinates(i,j + 1);
-                Cell thirdCell = board.getCellByCoordinates(i + 1 ,j + 1);
-                Cell fourthCell = board.getCellByCoordinates(i + 1, j);
+        for (int i = 1;i < board.getRowQuantity() ;i ++ ) {
+            for (int j = 1;j < board.getColumnQuantity() ;j ++ ) {
+                Cell firstCell = board.getCellByCoordinates(i - 1,j - 1);
+                Cell secondCell = board.getCellByCoordinates(i - 1,j - 1 + 1);
+                Cell thirdCell = board.getCellByCoordinates(i + 1 - 1 ,j + 1 - 1);
+                Cell fourthCell = board.getCellByCoordinates(i + 1 - 1, j - 1);
                 if ((this.isCircuite(firstCell,"/")) && (this.isCircuite(secondCell,"\\")) && (this.isCircuite(thirdCell,"/")
                         && (this.isCircuite(fourthCell, "\\")))) {
                     return false;
--- src/test/java/ar/fiuba/tdd/tp1/model/EndGameNoCyclesTest.java
+++ src/test/java/ar/fiuba/tdd/tp1/model/EndGameNoCyclesTest.java
@@ -14,26 +14,22 @@ public class EndGameNoCyclesTest {
 
     @Before
     public void setUp() throws Exception {
-        int dimRowS = 3;
-        int dimCols = 3;
+        int dimRowS = 5;
+        int dimCols = 5;
         this.oneBoard = new BoardRectangularWithRegions(dimCols,dimRowS);
                                   //ID                          //dATA  //NAME //isEDITABLE
-        this.oneBoard. setCellById(new Integer(5), new CellAlphabetical("/","1",true));
-        this.oneBoard. setCellById(new Integer(6), new CellAlphabetical("\\","8",true));
-        this.oneBoard. setCellById(new Integer(7), new CellAlphabetical("/","16",true));
-        this.oneBoard. setCellById(new Integer(8), new CellAlphabetical("\\","15",true));
-        this.oneBoard. setCellById(new Integer(9), new CellAlphabetical("/","12",true));
-        this.oneBoard. setCellById(new Integer(10), new CellAlphabetical("/","11",true));
-        this.oneBoard. setCellById(new Integer(11), new CellAlphabetical("\\","6",true));
-        this.oneBoard. setCellById(new Integer(12), new CellAlphabetical("/","5",true));
-        this.oneBoard. setCellById(new Integer(13), new CellAlphabetical("/","3",true));
+        this.oneBoard. setCellById(new Integer(1), new CellAlphabetical("/","1",true));
+        this.oneBoard. setCellById(new Integer(2), new CellAlphabetical("\\","2",true));
+        this.oneBoard. setCellById(new Integer(6), new CellAlphabetical("\\","6",true));
+        this.oneBoard. setCellById(new Integer(7), new CellAlphabetical("/","7",true));
         this.endGameCondition = new EndGameNoCycles();
+
 
     }
 
     @Test
     public void validate() throws Exception {
-        assertEquals(true,this.endGameCondition.validate(this.oneBoard));
+        assertFalse(this.endGameCondition.validate(this.oneBoard));
     }
 
 }
