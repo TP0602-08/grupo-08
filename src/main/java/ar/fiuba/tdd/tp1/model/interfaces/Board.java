0746febbe46efc952d5abbe3e1c65e10f197b6a3
marcosRiarte <mriarte@fi.uba.ar>
2016-11-08 05:14:58 -0300

    Descripiciones de las intefaces Boar y endgame condition

--- src/main/java/ar/fiuba/tdd/tp1/model/interfaces/Board.java
+++ src/main/java/ar/fiuba/tdd/tp1/model/interfaces/Board.java
@@ -6,7 +6,11 @@ import ar.fiuba.tdd.tp1.model.Region;
 
 import java.util.List;
 import java.util.Map;
-
+/*Esta interfaz tiene como objetivo representar al tablero del juego.Cada celda tiene su
+Id , y varias Ids forma una region.El tablero tendra un numero de celdas y una dimension
+(filas x columnas),ademas de un mapa que nos brinda los valores del tablero del tipo <id,valor>
+La idea de esta interfaz era ser extensible para varios tipos de tableros
+* */
 public interface Board {
     void apply(Move move);
 
