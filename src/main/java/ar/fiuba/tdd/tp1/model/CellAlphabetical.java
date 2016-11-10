feeaec768bdd7679598f3e3c5edb48aff5a01d66
marcosRiarte <mriarte@fi.uba.ar>
2016-11-10 15:32:19 -0300

    More Class Coments

--- src/main/java/ar/fiuba/tdd/tp1/model/CellAlphabetical.java
+++ src/main/java/ar/fiuba/tdd/tp1/model/CellAlphabetical.java
@@ -1,7 +1,14 @@
 package ar.fiuba.tdd.tp1.model;
 
 import ar.fiuba.tdd.tp1.model.interfaces.VisitorOfCell;
-
+/*This class implements the cell interface. It represents an alphabetic cell which will
+receive a string as a parameter that represents the cell ID (datanum) and a name (nameValue)
+It has the same cell methods
+IsEmpty: Returns true if the cell has a null object (it is empty)
+Accept: Use the board's Visit method
+DataToString: Also returns the value of the cell in string
+GetDatum: Returns the value of the cell in string
+In addition to the setDatum methods that allow us to set the value of the cell*/
 public class CellAlphabetical extends Cell {
     private String datum;
 
--- src/main/java/ar/fiuba/tdd/tp1/model/interfaces/EndGameCondition.java
+++ src/main/java/ar/fiuba/tdd/tp1/model/interfaces/EndGameCondition.java
@@ -1,8 +1,8 @@
 package ar.fiuba.tdd.tp1.model.interfaces;
 
 import ar.fiuba.tdd.tp1.model.BoardRectangularWithRegions;
-/*Esta interfaz representa las condicones de fin de juego.El metodo Validate al retornar un valor
-true,validará la condicion de fin juego.
+/*This interface represents the end of game conditions. The Validate method when returning a value
+True, will validate the game end condition.
 * */
 public interface EndGameCondition {
     Boolean validate(BoardRectangularWithRegions board);
