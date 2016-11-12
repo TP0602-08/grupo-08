b65e9057798099db242cf93d4b27b08b138f429c
marcosRiarte <mriarte@fi.uba.ar>
2016-11-12 03:39:24 -0300

    Images to wiki

--- src/main/java/ar/fiuba/tdd/tp1/model/CellNumerical.java
+++ src/main/java/ar/fiuba/tdd/tp1/model/CellNumerical.java
@@ -1,7 +1,14 @@
 package ar.fiuba.tdd.tp1.model;
 
 import ar.fiuba.tdd.tp1.model.interfaces.VisitorOfCell;
-
+/*This class implements the cell interface. It represents an numeric cell which will
+receive a Integer as a parameter that represents the cell ID (datanum) and a name (nameValue)
+It has the same cell methods
+IsEmpty: Returns true if the cell has a null object (it is empty)
+Accept: Use the board's Visit method
+DatumToString: Also returns the value of the cell in string
+GetDatum: Returns the value of the cell in string
+In addition to the setDatum methods that allow us to set the value of the cell*/
 public class CellNumerical extends Cell {
     private Integer datum;
 
